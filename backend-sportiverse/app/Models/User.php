7499bc6052f86f0deb2e22245e08275c1c7ec23a
Miguel Alonso Hernández <miguel.alonso.hernandez.ext@rsi.cajarural.com>
2024-02-18 09:32:57 +0100

    Se edita el modelo User

--- backend-sportiverse/app/Models/User.php
+++ backend-sportiverse/app/Models/User.php
@@ -47,4 +47,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function orderDetails()
+    {
+        return $this->hasManyThrough(OrderDetail::class, Order::class);
+    }
 }
