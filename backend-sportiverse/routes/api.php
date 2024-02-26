9e2ecc814d278236539f09ae6035300b4854edcd
Miguel Alonso Hernández <miguel.alonso.hernandez.ext@rsi.cajarural.com>
2024-02-26 17:15:09 +0100

    Se editan rutas

--- backend-sportiverse/routes/api.php
+++ backend-sportiverse/routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Public\ProductPublicController;
+use App\Http\Controllers\Public\CategoryPublicController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Admin\CategoryAdminController;
+use App\Http\Controllers\Admin\UserAdminController;
+use App\Http\Controllers\Admin\ProductAdminController;
+use App\Http\Controllers\Seller\ProductSellerController;
+use App\Http\Controllers\Customer\OrderCustomerController;
+use App\Http\Controllers\Customer\OrderDetailCustomerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +23,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Rutas v1
+Route::prefix('v1')->group(function () {
+    // Rutas públicas
+    Route::get('/public/products', [ProductPublicController::class, 'index']);
+    Route::get('/public/products/{product}', [ProductPublicController::class, 'show']);
+    Route::get('/public/categories', [CategoryPublicController::class, 'index']);
+    Route::get('/public/categories/{category}', [CategoryPublicController::class, 'show']);
+
+    // Rutas auth
+    Route::post('/auth/register', [AuthController::class, 'register']);
+    Route::post('/auth/login', [AuthController::class, 'login']);
+
+    // Rutas privadas
+    Route::middleware('auth:sanctum')->group(function () {
+        // Rutas que requieren autenticación
+
+        Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+        // role customer
+        Route::apiResource('/customer/orders', OrderCustomerController::class);
+
+        // role seller
+        Route::apiResource('/seller/products', ProductSellerController::class);
+
+        // role admin
+        Route::apiResource('/admin/users', UserAdminController::class);
+
+        Route::apiResource('/admin/categories', CategoryAdminController::class)->except([
+            'index', 'show',
+        ]);
+
+        Route::apiResource('/admin/products', ProductAdminController::class)->except([
+            'index', 'show', 'store'
+        ]);
+    });
 });
