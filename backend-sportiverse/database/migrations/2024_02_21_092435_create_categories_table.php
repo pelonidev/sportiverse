a6781e1994ae3dd430517257758b4e49dbd9f245
Miguel Alonso Hernández <miguel.alonso.hernandez.ext@rsi.cajarural.com>
2024-02-26 17:12:57 +0100

    Se añaden nuevas migraciones

--- backend-sportiverse/database/migrations/2024_02_21_092435_create_categories_table.php
+++ backend-sportiverse/database/migrations/2024_02_21_092435_create_categories_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug')->unique();
             $table->text('description')->nullable();
-            $table->unsignedBigInteger('category_id');
-            $table->decimal('price', 8, 2);
-            $table->integer('stock')->unsigned()->default(0);
             $table->string('image')->nullable();
             $table->timestamps();
         });
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
     }
 };
